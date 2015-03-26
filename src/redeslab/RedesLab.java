cae2df70b066db9de73d7d34da19f435ff2d1167
Mrls94 <sebasmrls94@gmail.com>
2015-03-26 18:57:05 -0500

    ProjectStarted

---
+++ src/redeslab/RedesLab.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package redeslab;
+
+/**
+ *
+ * @author Sebastian
+ */
+public class RedesLab {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        MainMenu a = new MainMenu();
+        a.show();
+        
+        
+    }
+}
